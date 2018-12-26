9be5ae20a9eb67fe32745736e4d53fbc041128e2
Francisco Muñoz <fmunoz@altotrack.com>
2018-12-26 15:27:28 -0300

    subiendo login y  sideMenu , cambiando paleta de colores primary y sec

---
+++ src/pages/login/login.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,MenuController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { HomePage } from '../home/home';
+
+/**
+ 
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  rut: AbstractControl;
+  loginForm: FormGroup;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              private menuCtrl:MenuController) {
+
+    this.loginForm = formBuilder.group({
+      rut:['',Validators.compose([Validators.required])]
+    })              
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  ionViewDidEnter(){
+    this.menuCtrl.enable(false);
+  }
+
+  ionViewWillLeave(){
+    this.menuCtrl.enable(true);
+  }
+
+  onSubmit(value:any){
+    console.log(value);
+
+    this.navCtrl.setRoot(HomePage);
+  }
+
+}
