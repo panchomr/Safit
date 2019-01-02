abafbfd7d65e732fdd181e7c19cf4c1784890876
Francisco Muñoz <fmunoz@altotrack.com>
2019-01-02 17:46:36 -0300

    protegiendo cambios en api

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthProvider {
 
-  private url ='http://www.safit.cl/api/Login'
+  private url ='http://www.safit.cl/v1/api/Login'
 
   constructor(public http: HttpClient) {
     console.log('Hello AuthProvider Provider');
